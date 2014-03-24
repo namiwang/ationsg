56a2b91bc27834ef8b7287380e3d40d5b7f5ca51
namiheike <namiheike@gmail.com>
2014-03-24 17:08:42 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ationsg::Application.config.secret_key_base = '6ac4c80fd1aeb4ab993e3f81a57932ed941b42aac25feb3f55cf501df6ab8a5d243a0baf28c129e882f4ebf2ff1c9b5216d3fb07ad5255aeb8f4ddc94c7a7cea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ationsg::Application.config.session_store :cookie_store, key: '_ationsg_session'
