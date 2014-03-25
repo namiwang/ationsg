b5c981a402b55ab4c335d4bfe6b8670184d6cc50
namiheike <namiheike@gmail.com>
2014-03-25 15:37:37 +0800

    basic devise view

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Ationsg
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'zh-CN'.to_sym
+
+    config.to_prepare do
+      Devise::SessionsController.layout 'devise'
+      Devise::RegistrationsController.layout 'devise'
+    end
+
   end
 end
