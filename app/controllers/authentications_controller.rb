961c9604aa7969cef01b3507f7d071ccc78457f6
namiheike <namiheike@gmail.com>
2014-04-12 03:45:54 -0400

    oauth view fix

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -8,10 +8,10 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
   def authorize(provider)
     session[:oauth] = env['omniauth.auth']
     if @user = Authentication.find_user(provider, env['omniauth.auth']['uid'])
-      (flash[:notice] ||= []) << I18n.t 'devise.sessions.signed_in'
+      (flash[:notice] ||= []) << (I18n.t 'devise.sessions.signed_in')
       sign_in_and_redirect @user, :event => :authentication
     else
-      (flash[:notice] ||= []) << I18n.t 'devise.omniauth_callbacks.success', kind: (I18n.t "devise.oauth_services.providers.#{provider}")
+      (flash[:notice] ||= []) << (I18n.t 'devise.omniauth_callbacks.success', kind: (I18n.t "devise.authentications.providers.#{provider}_with_icon").html_safe).html_safe
       redirect_to oauth_bind_new_path
     end
   end
