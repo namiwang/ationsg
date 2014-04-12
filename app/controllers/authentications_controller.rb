777c2305110dd6b3af95c26d502b4caac6463ef8
namiheike <namiheike@gmail.com>
2014-04-12 03:36:39 -0400

    flash

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -8,12 +8,10 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
   def authorize(provider)
     session[:oauth] = env['omniauth.auth']
     if @user = Authentication.find_user(provider, env['omniauth.auth']['uid'])
-      # TODO make flash work in layout
-      # flash[:notice] = I18n.t 'devise.sessions.signed_in'
+      (flash[:notice] ||= []) << I18n.t 'devise.sessions.signed_in'
       sign_in_and_redirect @user, :event => :authentication
     else
-      # TODO make flash work in layout    
-      # flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: (I18n.t "devise.oauth_services.providers.#{provider}")
+      (flash[:notice] ||= []) << I18n.t 'devise.omniauth_callbacks.success', kind: (I18n.t "devise.oauth_services.providers.#{provider}")
       redirect_to oauth_bind_new_path
     end
   end
