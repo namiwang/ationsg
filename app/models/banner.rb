1cfa569136434574fe5b31f781aaf027489a2346
namiheike <namiheike@gmail.com>
2014-06-10 16:30:25 +0800

    little works

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,6 +1,6 @@
 class Banner < ActiveRecord::Base
   # associations
-  has_many :images, as: :imageable
+  has_many :images, as: :imageable # TODO should use has_one but cannot save via activeadmin
   accepts_nested_attributes_for :images
 
   def image
